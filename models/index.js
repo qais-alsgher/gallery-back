8c5532c530c9e7a0d8335ef60613dbf80271639a
qais-alsgher <qaisalsgher@gmail.com>
2022-09-28 12:52:15 +0000

    add url postgres and add option

--- models/index.js
+++ models/index.js
@@ -4,18 +4,18 @@ const { Sequelize, DataTypes } = require('sequelize');
 
 const db = {};
 
-// const sequelizeOption = {
-//     dialectOptions: {
-//         ssl: {
-//             require: true,
-//             rejectUnauthorized: false
-//         }
-//     }
-// };
+const sequelizeOption = {
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    }
+};
 
-const POSTGRES_URL = 'postgres://localhost:5432/postgres';
+const POSTGRES_URL = process.env.DATABASE_URL || 'postgres://localhost:5432/postgres';
 
-const sequelize = new Sequelize(POSTGRES_URL);
+const sequelize = new Sequelize(POSTGRES_URL, sequelizeOption);
 
 db.sequelize = sequelize;
 db.gallary = require('./gallery')(sequelize, DataTypes);
